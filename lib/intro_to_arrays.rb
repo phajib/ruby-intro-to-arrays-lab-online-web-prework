b01f354034ceaf82d0538a56f1024fed39c1bfa8
H B <phajib@gmail.com>
2019-01-16 23:18:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,15 @@
+def instantiate_new_array
+  my_new_array = []
+  return my_new_array
+end
+
+def array_with_two_elements
+  my_two_array = ["Australian", "Open"]
+  return my_two_array
+end
+
+def first_element(array)
+  
+  array = first_element
+  return first_element.first
+end
